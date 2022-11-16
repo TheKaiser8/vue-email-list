7ce623886ddca92546efd4e74c7afaa6ba4816d9
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-11-16 17:15:57 +0100

    Aggiunge ciclo for che genera lista mail Ciclo v-for stampa la lista mail in pagina

--- js/script.js
+++ js/script.js
@@ -13,6 +13,15 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            mailList: []
+        }
+    },
+    created() {
+        for (let i = 1; i <= 10; i++) {
+            axios.get('https:flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+            this.mailList.push(response.data.response);
+        })
         }
     }
 }).mount('#app');
