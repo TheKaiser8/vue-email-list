29a58dcb647f40d185c1137428270858f17b2c11
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-11-16 17:48:01 +0100

    Corregge indirizzo API

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ createApp({
     },
     created() {
         for (let i = 1; i <= 10; i++) {
-            axios.get('https:flynn.boolean.careers/exercises/api/random/mail')
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
             this.mailList.push(response.data.response);
             })
